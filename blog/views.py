719603a8005ddbf952a3b635c96139e9e6e4a439
Pizanias <perolocooo@hotmail.com>
2022-07-22 18:42:44 +0300

    My blog main

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,60 @@
+from datetime import date
 from django.shortcuts import render
 
+
+posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "Indy-and-Henry-in-an-airplane.png",
+        "author": "Stefanos Pizanias",
+        "date": date(2022, 7, 22),
+        "title": "Mountain Hiking",
+        "excerpt": "There is nothing like the views you get when hiking in the mountains!",
+        "content": "HUZZAAAA HUZZZAAAAA HUZZZAAAAA1"
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "indiana-jones-et-la-dern-ii02-g.jpg",
+        "author": "Stefanos Pizanias",
+        "date": date(2022, 5, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code?",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "Holy-Grail.jpg",
+        "author": "Stefanos Pizanias",
+        "date": date(2022, 6, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
 # Create your views here.
 
 
@@ -11,6 +66,5 @@ def posts(request):
     return render(request, "blog/all-posts.html")
 
 
-def post_detail(request):
-    pass
-
+def post_detail(request, slug):
+    return render(request, "blog/post-detail.html")
